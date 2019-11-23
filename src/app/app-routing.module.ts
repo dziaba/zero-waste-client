c08597549fd2cbbc735d629d0eddffdabcb49d4f
Edamski <11damx@gmail.com>
2019-11-23 11:21:43 +0100

    v3 kurwa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
+import { LoginComponent } from './page/login/login.component';
+import { HomeComponent } from './page/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './page/contact/contact.component';
+import { RecommendComponent } from './page/home/recommend/recommend.component';
+import { KnowladgeComponent } from './page/home/knowladge/knowladge.component';
+import { ZeroWasteComponent } from './page/home/zero-waste/zero-waste.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'recommend', component: RecommendComponent },
+  { path: 'knowladge', component: KnowladgeComponent },
+  { path: 'zero-waste', component: ZeroWasteComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
+import { HeaderModule } from './page/common/header/header.module';
+import { LoginModule } from './page/login/login.module';
+import { ContactModule } from './page/contact/contact.module';
+import { HomeModule } from './page/home/home.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatInputModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-<<<<<<< Updated upstream
-    AppRoutingModule
-=======
     AppRoutingModule,
     HomeModule,
     LoginModule,
@@ -32,7 +42,6 @@ import { AppComponent } from './app.component';
   ],
   exports: [
     TranslateModule
->>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
