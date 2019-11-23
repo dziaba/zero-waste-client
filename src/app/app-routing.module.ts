453a25a6835e7bd18576ee2fc6a6a7e949cfd3a8
Dziaba <k.lison@wp.pl>
2019-11-23 04:19:30 +0100

    Widoki

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { LoginComponent } from './page/login/login.component';
 import { HomeComponent } from './page/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecommendComponent } from './page/home/recommend/recommend.component';
+import { KnowladgeComponent } from './page/home/knowladge/knowladge.component';
+import { ZeroWasteComponent } from './page/home/zero-waste/zero-waste.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'start', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'recommend', component: RecommendComponent },
+  { path: 'knowladge', component: KnowladgeComponent },
+  { path: 'zero-waste', component: ZeroWasteComponent},
   { path: 'login', component: LoginComponent },
   { path: '**', component: HomeComponent }
 ];
